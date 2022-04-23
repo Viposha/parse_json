c9788c5bbc593489775723abc2c23077dd8569dd
Vitalii Hoshchenko <viposha@gmail.com>
2022-04-23 14:56:06 +0300

    parsing to json file install.info

--- parse.py
+++ parse.py
@@ -1,28 +1,32 @@
 import requests
 import json
 
+# download all packages api
 response = requests.get('https://formulae.brew.sh/api/formula.json')
 packages_json = response.json()
 
-packages_name = packages_json[0]['name']
-#print(packages_name)
+result = []
+for package in packages_json[:5]:
+    a = 0
+    package_name = package['name']
+    package_url = f'https://formulae.brew.sh/api/formula/{package_name}.json'
+    response2 = requests.get(package_url)
+    package_json = response2.json()
+    a += 1
+    package_dict = {
+                'name': package_json['name'],
+                'desc': package_json['desc'],
+                'intalls_30d': package_json['analytics']['install']['30d'][package_name],
+                'intalls_90d': package_json['analytics']['install']['90d'][package_name],
+                'intalls_365d': package_json['analytics']['install']['365d'][package_name]
+                    }
+    result.append(package_dict)
+    if a == 5:
+        break
+with open('install_info', 'w') as file:
+    json.dump(result, file, indent=2)
 
-big_data = json.dumps(packages_json[0], indent=2)
 
-
-
-response2 = requests.get('https://formulae.brew.sh/api/formula/a2ps.json')
-package_json = response2.json()
-
-package_name = package_json['name']
-package_desc = package_json['desc']
-package_intalls_30d = package_json['analytics']['install']['30d']['a2ps']
-package_intalls_90d = package_json['analytics']['install']['90d']['a2ps']
-package_intalls_365d = package_json['analytics']['install']['365d']['a2ps']
-#print(package_intalls_365d)
-#print(package_json['name'])
-one_package = json.dumps(package_json, indent=2)
-#print(one_package)
 
 
 
